70b5448731e47b9ef34343cf7c21d0dbba4e9284
Migssil <168006010+Migssil@users.noreply.github.com>
2024-12-17 19:48:18 -0300

    Update Menu.lua

--- Menu.lua
+++ Menu.lua
@@ -29,4 +29,12 @@ entrarbt.MouseButton1Click:Connect(function()
     print("Botão foi pressionado!")  -- Mensagem no console  -- Mudar o texto do botão
     entrarbt.BackgroundColor3 = Color3.fromRGB(100, 200, 100)  -- Mudar a cor (verde claro).
     entrarbt:Destroy()
+    local HomeMenuBotao = Instance.new("TextButton"
+    HomeMenuBotao.Size = UDim2.new(0.3, 0, 0.15, 0)
+    HomeMenuBotao.Position = UDim2.new(0, 0, 0, 0)  -- Posicionado dentro do Frame
+    HomeMenuBotao.BackgroundColor3 = Color3.fromRGB(50, 50, 50)  -- Cinza meio escuro
+    HomeMenuBotao.Text = "Entrar"  -- Texto no botão
+    HomeMenuBotao.TextSize = 20  -- Tamanho do texto
+    HomeMenuBotao.TextColor3 = Color3.fromRGB(255, 255, 255)  -- Cor branca para o texto
+    HomeMenuBotao.Parent = frame
 end)
