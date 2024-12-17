1096025c609752b034d30b37701f176906b89bdd
Migssil <168006010+Migssil@users.noreply.github.com>
2024-12-17 19:55:09 -0300

    Update Menu.lua

--- Menu.lua
+++ Menu.lua
@@ -33,7 +33,7 @@ entrarbt.MouseButton1Click:Connect(function()
     HomeMenuBotao.Size = UDim2.new(0.3, 0, 0.15, 0)
     HomeMenuBotao.Position = UDim2.new(0, 0, 0, 0)  -- Posicionado dentro do Frame
     HomeMenuBotao.BackgroundColor3 = Color3.fromRGB(50, 50, 50)  -- Cinza mais escuro
-    HomeMenuBotao.Text = "Entrar"  -- Texto no botão
+    HomeMenuBotao.Text = "Home"  -- Texto no botão
     HomeMenuBotao.TextSize = 20  -- Tamanho do texto
     HomeMenuBotao.TextColor3 = Color3.fromRGB(255, 255, 255)  -- Cor branca para o texto
     HomeMenuBotao.Parent = frame
